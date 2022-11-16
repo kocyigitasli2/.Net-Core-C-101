9f430e8132145dbcc025d4832828dc1d84a6dc38
Aslı Koçyiğit <33804678+kocyigitasli2@users.noreply.github.com>
2022-11-16 18:35:50 +0300

    Add files via upload

---
+++ console-programlama/console-programlama/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace console_programlama
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+        {
+           System.Console.WriteLine("Hello World!");
+           System.Console.WriteLine("HI!");
+
+            Console.WriteLine("adınızı giriniz:  ");
+            string name = Console.ReadLine();
+            Console.WriteLine("soyadınızı giriniz:  ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("teşekkürler, " + " MERHABA " + name + " " + surname + " " + ":) " );
+
+        }
+    }
+}
